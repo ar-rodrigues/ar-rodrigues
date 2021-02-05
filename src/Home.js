01ae159248aedc5dbeec3aff81aa0a765dc775af
Alisson Rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2021-02-05 10:40:00 -0300

    Home background

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 function Home() {
+  const imageBgOne = "/grey-2661270.png";
+  const imageBgTwo = "/back-to-work.jpg";
   return (
-    <header id="home" className="header">
-      {
-        //}<img src='https://images.unsplash.com/photo-1522648485144-849409408aee?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ' />
-      }
+    <header 
+    id="home" 
+    className="header"
+    style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.35)), url(${imageBgOne}), url(${imageBgTwo})`}}
+    >
       <div className="header-box">
         <h3>Alisson Rodrigues</h3>
         <h4>Estudante de Ciências Sociais (UFRGS)</h4>

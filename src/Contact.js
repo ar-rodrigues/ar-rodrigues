f051f50f4765c603dc484914280b01cc08a5c6b8
Alisson Rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2021-03-21 21:49:34 -0300

    Form

--- src/Contact.js
+++ src/Contact.js
@@ -9,119 +9,114 @@ const idGen = () => {
 function Contact() {
   const [fields, setFields] = useState({
     id: "",
-    data:"",
+    data: "",
     name: "",
     email: "",
-    message: "",
+    message: ""
   });
 
-  const handleChange = (e)=> {
+  const handleChange = (e) => {
     let name = e.target.name;
     const value = e.target.value;
-    setFields({...fields,
-      id: idGen(),
-      [name]: value,
-    })
-  }
-  const handleSubmit = (e)=>{
+    setFields({ ...fields, id: idGen(), [name]: value });
+  };
+  const handleSubmit = (e) => {
     e.preventDefault();
     setFields(fields);
     setFields({
       id: "",
-      data:"",
+      data: "",
       name: "",
       email: "",
-      message: "",
-    })
-  }
+      message: ""
+    });
+  };
 
-
-  
   return (
     <section className="contact-box">
-        <h3>
-          Quer saber mais? <br /> Entre em contato comigo!
-        </h3>
-        <form
-          id="contact"
-          className="contact-form"
-          method="POST"
-          netlify-honeypot="data-field"
-          data-netlify="true"
-          onSubmit={(e)=>handleSubmit(e)}
-        >
-          <div className="form-itens">
-            {/*H O N E Y P O T */}
-            <div className="honey">
-              <label htmlFor="data"></label>
-              <input
-                name="data-field"
-                autoComplete="off"
-                type="text"
-                id="data"
-                value={fields.data}
-                onChange={(e)=>handleChange(e)}
-              />
-            </div>
+      <h3>
+        Quer saber mais? <br /> Entre em contato comigo!
+      </h3>
+      <form
+        id="contact"
+        className="contact-form"
+        method="POST"
+        netlify-honeypot="data-field"
+        onSubmit={(e) => handleSubmit(e)}
+        data-netlify="true"
+      >
+        <div className="form-itens">
+          {/*H O N E Y P O T */}
+          <div className="honey">
+            <label htmlFor="data"></label>
+            <input
+              name="data-field"
+              autoComplete="off"
+              type="text"
+              id="data"
+              value={fields.data}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
 
-            <label htmlFor="name" id="name" className="form-label">
-              Nome:
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={fields.name}
-              onChange={(e)=>handleChange(e)}
-              className="form-input"
-              name="name"
-              placeholder="digite  aqui seu nome"
-              required
-            />
-          </div>
-          <div className="form-itens">
-            <label htmlFor="email" id="email" className="form-label">
-              Email:
-            </label>
-            <input
-              type="text"
-              id="email"
-              value={fields.email}
-              onChange={(e)=>handleChange(e)}
-              className="form-input"
-              name="email"
-              placeholder="email@endereço.com"
-              pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-              required
-            />
-          </div>
-          <textarea
-            className="comments"
-            name="message"
-            id="message"
-            value={fields.message}
-            onChange={(e)=>handleChange(e)}
-            placeholder="dúvidas, sugestões, conselhos..."
-            rows="5"
-            cols="50"
+          <label htmlFor="name" id="name" className="form-label">
+            Nome:
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={fields.name}
+            onChange={(e) => handleChange(e)}
+            className="form-input"
+            name="name"
+            placeholder="digite  aqui seu nome"
             required
           />
+        </div>
+        <div className="form-itens">
+          <label htmlFor="email" id="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="text"
+            id="email"
+            value={fields.email}
+            onChange={(e) => handleChange(e)}
+            className="form-input"
+            name="email"
+            placeholder="email@endereço.com"
+            pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            required
+          />
+        </div>
+        <textarea
+          className="comments"
+          name="message"
+          id="message"
+          value={fields.message}
+          onChange={(e) => handleChange(e)}
+          placeholder="dúvidas, sugestões, conselhos..."
+          rows="5"
+          cols="50"
+          required
+        />
 
-          <button className="form-button" type="submit">
-            Enviar
-          </button>
-        </form>
-        <ul className="social-contact">
-          {social.map((socialIcon) => {
-            const { id, url, icon, name } = socialIcon;
-            return (
-              <li key={id}>
-                <a href={url}>{icon}</a>
-                <h4>{name}</h4>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
+        <button className="form-button" type="submit">
+          Enviar
+        </button>
+      </form>
+      <ul className="social-contact">
+        {social.map((socialIcon) => {
+          const { id, url, icon, name } = socialIcon;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+              <h4>{name}</h4>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
